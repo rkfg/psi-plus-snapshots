5eb3dd75363567e0abc38d78caa79ec60d9347f9
rkfg <rkfg@rkfg.me>
2016-09-19 18:14:46 +0300

    Remove garbage, optimize url search.

--- src/plugins/generic/imagepreviewplugin/imagepreviewplugin.cpp
+++ src/plugins/generic/imagepreviewplugin/imagepreviewplugin.cpp
@@ -21,10 +21,8 @@
 #include "psiplugin.h"
 #include "activetabaccessinghost.h"
 #include "activetabaccessor.h"
-#include "toolbariconaccessor.h"
 #include "iconfactoryaccessor.h"
 #include "iconfactoryaccessinghost.h"
-#include "gctoolbariconaccessor.h"
 #include "stanzasender.h"
 #include "stanzasendinghost.h"
 #include "accountinfoaccessinghost.h"
@@ -53,15 +51,10 @@
 #include <QImageReader>
 #include <QTextDocumentFragment>
 
-#define constVersion "0.1.2"
-
-#define CONST_LAST_FOLDER "lastfolder"
-
-const int MAX_SIZE = 400;
+#define constVersion "0.1.0"
 
 class ImagePreviewPlugin: public QObject,
 		public PsiPlugin,
-		public ToolbarIconAccessor,
 		public IconFactoryAccessor,
 		public ActiveTabAccessor,
 		public PluginInfoProvider,
@@ -71,7 +64,7 @@ Q_OBJECT
 #ifdef HAVE_QT5
 	Q_PLUGIN_METADATA(IID "com.psi-plus.ImagePreviewPlugin")
 #endif
-Q_INTERFACES(PsiPlugin ToolbarIconAccessor
+Q_INTERFACES(PsiPlugin
 		IconFactoryAccessor ActiveTabAccessor
 		PluginInfoProvider OptionAccessor ChatTabAccessor)
 public:
@@ -87,16 +80,7 @@ public:
 	}
 	virtual void restoreOptions() {
 	}
-	virtual QList<QVariantHash> getButtonParam();
-	virtual QAction* getAction(QObject*, int, const QString&) {
-		return 0;
-	}
-	virtual QList<QVariantHash> getGCButtonParam();
-	virtual QAction* getGCAction(QObject*, int, const QString&) {
-		return 0;
-	}
 	virtual void setIconFactoryAccessingHost(IconFactoryAccessingHost* host);
-	virtual void setStanzaSendingHost(StanzaSendingHost *host);
 	virtual void setActiveTabAccessingHost(ActiveTabAccessingHost* host);
 	virtual void setAccountInfoAccessingHost(AccountInfoAccessingHost* host);
 	virtual void setPsiAccountControllingHost(PsiAccountControllingHost *host);
@@ -108,21 +92,20 @@ public:
 	virtual void setupChatTab(QWidget*, int, const QString&) {
 	}
 	virtual void setupGCTab(QWidget* widget, int, const QString&) {
-		//GCMainDlg* dlg = qobject_cast<GCMainDlg*>(widget);
 		connect(widget, SIGNAL(messageAppended(const QString &, QTextEdit*)),
 				this, SLOT(messageAppended(const QString &, QTextEdit*)));
 	}
 
-	virtual bool appendingChatMessage(int account, const QString& contact,
-			QString& body, QDomElement& html, bool local);
+	virtual bool appendingChatMessage(int, const QString&, QString&,
+			QDomElement&, bool) {
+		return false;
+	}
 
 private slots:
-	void actionActivated();
 	void messageAppended(const QString &, QTextEdit*);
 	void imageReply(QNetworkReply* reply);
 private:
 	IconFactoryAccessingHost* iconHost;
-	StanzaSendingHost* stanzaSender;
 	ActiveTabAccessingHost* activeTab;
 	AccountInfoAccessingHost* accInfo;
 	PsiAccountControllingHost *psiController;
@@ -139,7 +122,7 @@ Q_EXPORT_PLUGIN(ImagePreviewPlugin)
 #endif
 
 ImagePreviewPlugin::ImagePreviewPlugin() :
-		iconHost(0), stanzaSender(0), activeTab(0), accInfo(0), psiController(
+		iconHost(0), activeTab(0), accInfo(0), psiController(
 				0), psiOptions(0), enabled(false), manager(
 				new QNetworkAccessManager(this)) {
 	connect(manager, SIGNAL(finished(QNetworkReply *)), this,
@@ -192,21 +175,6 @@ QWidget* ImagePreviewPlugin::options() {
 	return optionsWid;
 }
 
-QList<QVariantHash> ImagePreviewPlugin::getButtonParam() {
-	QVariantHash hash;
-	hash["tooltip"] = QVariant(tr("Send Image"));
-	hash["icon"] = QVariant(QString("imagepreviewplugin/icon"));
-	hash["reciver"] = qVariantFromValue(qobject_cast<QObject *>(this));
-	hash["slot"] = QVariant(SLOT(actionActivated()));
-	QList<QVariantHash> l;
-	l.push_back(hash);
-	return l;
-}
-
-QList<QVariantHash> ImagePreviewPlugin::getGCButtonParam() {
-	return getButtonParam();
-}
-
 void ImagePreviewPlugin::setAccountInfoAccessingHost(
 		AccountInfoAccessingHost* host) {
 	accInfo = host;
@@ -226,102 +194,9 @@ void ImagePreviewPlugin::setOptionAccessingHost(OptionAccessingHost *host) {
 	psiOptions = host;
 }
 
-void ImagePreviewPlugin::setStanzaSendingHost(StanzaSendingHost *host) {
-	stanzaSender = host;
-}
-
 void ImagePreviewPlugin::setActiveTabAccessingHost(
 		ActiveTabAccessingHost* host) {
 	activeTab = host;
-}
-
-void ImagePreviewPlugin::actionActivated() {
-	if (!enabled)
-		return;
-
-	QString fileName("");
-	QString jid = activeTab->getYourJid();
-	QString jidToSend = activeTab->getJid();
-	int account = 0;
-	QString tmpJid("");
-	while (jid != (tmpJid = accInfo->getJid(account))) {
-		++account;
-		if (tmpJid == "-1")
-			return;
-	}
-
-	QMenu m;
-	QList<QAction*> list;
-	list << new QAction(tr("Open file"), &m)
-			<< new QAction(tr("From clipboard"), &m);
-	QAction *act = m.exec(list, QCursor::pos());
-	if (!act)
-		return;
-
-	if ("offline" == accInfo->getStatus(account)) {
-		return;
-	}
-
-	QPixmap pix;
-	QString imageName;
-	if (list.indexOf(act) == 1) {
-		if (!QApplication::clipboard()->mimeData()->hasImage())
-			return;
-
-		pix = QPixmap::fromImage(QApplication::clipboard()->image());
-		imageName = QApplication::clipboard()->text();
-	} else {
-		const QString lastPath = psiOptions->getPluginOption(CONST_LAST_FOLDER,
-				QDir::homePath()).toString();
-		fileName = QFileDialog::getOpenFileName(0, tr("Open Image"), lastPath,
-				tr("Images (*.png *.gif *.jpg *.jpeg *.ico)"));
-		if (fileName.isEmpty())
-			return;
-
-		QFile file(fileName);
-		if (file.open(QIODevice::ReadOnly)) {
-			pix = QPixmap::fromImage(QImage::fromData(file.readAll()));
-			imageName = QFileInfo(file).fileName();
-
-		} else {
-			return;
-		}
-		psiOptions->setPluginOption(CONST_LAST_FOLDER, QFileInfo(file).path());
-	}
-
-	QByteArray image;
-	QString mimeType("jpeg");
-	if (pix.height() > MAX_SIZE || pix.width() > MAX_SIZE) {
-		pix = pix.scaled(MAX_SIZE, MAX_SIZE, Qt::KeepAspectRatio,
-				Qt::SmoothTransformation);
-	}
-	QBuffer b(&image);
-	pix.save(&b, mimeType.toLatin1().constData());
-	QString imageBase64(image.toBase64());
-	int length = image.length();
-	if (length > 61440) {
-		QMessageBox::information(0, tr("The image size is too large."),
-				tr("Image size must be less than 60 kb"));
-	}
-	QString mType =
-			QLatin1String(sender()->parent()->metaObject()->className())
-					== "PsiChatDlg" ? "chat" : "groupchat";
-	QString body = tr("Image %1 bytes received.").arg(QString::number(length));
-	QString msgHtml = QString("<message type=\"%1\" to=\"%2\" id=\"%3\" >"
-			"<body>%4</body>"
-			"<html xmlns=\"http://jabber.org/protocol/xhtml-im\">"
-			"<body xmlns=\"http://www.w3.org/1999/xhtml\">"
-			"<br/><img src=\"data:image/%5;base64,%6\" alt=\"img\"/> "
-			"</body></html></message>").arg(mType).arg(jidToSend).arg(
-			stanzaSender->uniqueId(account)).arg(body).arg(mimeType).arg(
-			imageBase64);
-
-	stanzaSender->sendStanza(account, msgHtml);
-	psiController->appendSysMsg(account, jidToSend,
-			tr(
-					"Image %1 sent <br/><img src=\"data:image/%2;base64,%3\" alt=\"img\"/> ").arg(
-					imageName).arg(mimeType).arg(imageBase64));
-
 }
 
 QString ImagePreviewPlugin::pluginInfo() {
@@ -333,29 +208,17 @@ QPixmap ImagePreviewPlugin::icon() const {
 	return QPixmap(":/imagepreviewplugin/imagepreviewplugin.gif");
 }
 
-bool ImagePreviewPlugin::appendingChatMessage(int account,
-		const QString& contact, QString& body, QDomElement& html, bool local) {
-	qDebug() << "Appending" << contact << ":" << body << " / "
-			<< html.toDocument().toString();
-	return false;
-}
-
-void ImagePreviewPlugin::messageAppended(const QString & message,
-		QTextEdit* te_log) {
+void ImagePreviewPlugin::messageAppended(const QString &, QTextEdit* te_log) {
 	if (!enabled) {
 		return;
 	}
-	qDebug() << "RECEIVED:" << message;
 	auto cur = te_log->textCursor();
 	te_log->moveCursor(QTextCursor::End, QTextCursor::MoveAnchor);
-	while (te_log->find("http", QTextDocument::FindBackward)) {
-		te_log->moveCursor(QTextCursor::EndOfLine, QTextCursor::KeepAnchor);
-		auto url = QString(te_log->textCursor().selectedText()).replace(
-				QRegExp("(https?://\\S*)\\s?.*$"), "\\1");
+	te_log->moveCursor(QTextCursor::StartOfLine, QTextCursor::MoveAnchor);
+	QTextCursor found = te_log->textCursor();
+	while (!(found = te_log->document()->find(QRegExp("https?://\\S*"), found)).isNull()) {
+		auto url = found.selectedText();
 		qDebug() << "URL FOUND:" << url;
-		if (failed.contains(url)) {
-			break;
-		}
 		if (!pending.contains(url)) {
 			pending.insert(url);
 			QNetworkRequest req;
@@ -363,7 +226,7 @@ void ImagePreviewPlugin::messageAppended(const QString & message,
 			req.setOriginatingObject(te_log);
 			manager->head(req);
 		}
-	}
+	};
 	te_log->setTextCursor(cur);
 }
 
