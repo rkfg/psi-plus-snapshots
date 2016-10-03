3a5fcfbbf4b7ee334b8c8c479d1acc77b983539e
rkfg <rkfg@rkfg.me>
2016-10-03 22:54:22 +0300

    Optimizations.

--- src/plugins/generic/imagepreviewplugin/imagepreviewplugin.cpp
+++ src/plugins/generic/imagepreviewplugin/imagepreviewplugin.cpp
@@ -42,6 +42,7 @@
 #include <QCheckBox>
 #include <QWebView>
 #include <QWebFrame>
+#include <QWebElementCollection>
 
 #define constVersion "0.1.0"
 #define sizeLimitName "imgpreview-size-limit"
@@ -203,14 +204,19 @@ void ImagePreviewPlugin::messageAppended(const QString &, QWidget* logWidget) {
 	} else {
 		QWebView* wv_log = qobject_cast<QWebView*>(logWidget);
 		QRegExp urlRE("(https?://\\S*)");
-		QString src = wv_log->page()->mainFrame()->toPlainText();
-		int offset = 0;
-		int found = 0;
-		while ((found = urlRE.indexIn(src, offset)) >= 0) {
-			auto url = urlRE.cap(1);
-			qDebug() << "URL FOUND:" << url;
-			queueUrl(url, wv_log);
-			offset = found + urlRE.matchedLength();
+		QWebFrame* mainFrame = wv_log->page()->mainFrame();
+		auto elems = mainFrame->findAllElements("a[href]");
+		for (auto i = elems.constEnd() - 1;; i--) {
+			if ((*i).isNull()) {
+				break;
+			}
+			// skip already processed anchors
+			if ((*i).firstChild().tagName() != "img") {
+				auto url = (*i).attribute("href", "");
+				if (url.startsWith("http://") || url.startsWith("https://")) {
+					queueUrl(url, wv_log);
+				}
+			}
 		}
 	}
 }
@@ -263,13 +269,11 @@ void ImagePreviewPlugin::imageReply(QNetworkReply* reply) {
 				image.save(&imageBuf, "jpg", 60);
 				QWebView* wv_log = qobject_cast<QWebView*>(reply->request().originatingObject());
 				QWebFrame* mainFrame = wv_log->page()->mainFrame();
-				mainFrame->evaluateJavaScript(QString("var links = document.body.getElementsByTagName('a');"
+				mainFrame->evaluateJavaScript(QString("var links = document.body.querySelectorAll('a[href=\"%1\"]');"
 						"for (var i = 0; i < links.length; i++) {"
 						"  var elem = links[i];"
-						"  if(elem.getAttribute('href') == '"+ urlStr +"') {"
-						"    elem.innerHTML = \"<img src='data:image/jpeg;base64,%1'/>\";"
-						"  }"
-						"}").arg(QString(imageBytes.toBase64())));
+						"  elem.innerHTML = \"<img src='data:image/jpeg;base64,%2'/>\";"
+						"}").arg(urlStr).arg(QString(imageBytes.toBase64())));
 			}
 		} catch (std::exception& e) {
 			qWarning() << "ERROR: " << e.what();
